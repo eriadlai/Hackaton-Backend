9720de3cb8c7650a9185849a410233124949e14d
Adlai CG <42752044+eriadlai@users.noreply.github.com>
2022-11-12 13:20:18 -0800

    adicion de CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var cors = require('cors')
 const bodyParser = require("body-parser");
 const oMaterial = require('./src/components/material');
 const oUsuario = require('./src/components/usuario');
@@ -7,7 +8,7 @@ const oManualidad = require('./src/components/manualidad');
 const oCategoria = require('./src/components/categoria');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(cors())
 app.use("/API/Reciclaje", oMaterial)
 app.use("/API/Reciclaje", oUsuario)
 app.use("/API/Reciclaje", oManualidad)
